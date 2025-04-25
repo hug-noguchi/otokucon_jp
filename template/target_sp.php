e267c267b7a21536635663745471ffc7a644ad3f
keito noguchi <noguchi@hug-inc.co.jp>
2025-04-25 20:19:37 +0900

    5月前半タイマー・日付設定

--- template/target_sp.php
+++ template/target_sp.php
@@ -1,5 +1,5 @@
-<?php if (strtotime(date('Y-m-d H:i')) < strtotime('2025-4-15 15:00')) { ?>
-  2025.4.15
-<?php } else { ?>
+<?php if (strtotime(date('Y-m-d H:i')) < strtotime('2025-4-30 15:00')) { ?>
   2025.4.30
+<?php } else { ?>
+  2025.5.15
 <?php } ?>
