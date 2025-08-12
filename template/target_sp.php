02cfd349afd6d348e5e690f2cbf7a223fbba0777
keito noguchi <noguchi@hug-inc.co.jp>
2025-08-12 19:14:17 +0900

    タイマー・日付設定

--- template/target_sp.php
+++ template/target_sp.php
@@ -1,5 +1,5 @@
-<?php if (strtotime(date('Y-m-d H:i')) < strtotime('2025-7-31 15:00')) { ?>
-  2025.7.31
-<?php } else { ?>
+<?php if (strtotime(date('Y-m-d H:i')) < strtotime('2025-8-15 15:00')) { ?>
   2025.8.15
+<?php } else { ?>
+  2025.8.31
 <?php } ?>
