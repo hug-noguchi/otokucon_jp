5b2dbcb8e26aa95c24147fb98613fdd1e153d481
keito noguchi <noguchi@hug-inc.co.jp>
2025-10-16 16:34:52 +0900

    10/15 カウントダウン部分をテンプレートファイル化（template/countdown.php）・キャンペーン日付調整

--- index_test.php
+++ index_test.php
@@ -81,28 +81,7 @@ get_header(); ?>
       </p>
     </div>
   </a>
-  <div class="number_apply add-control">
-    <div class="action-close">
-      <input type="checkbox" class="checkbox" id="close" name="close" /><label for="close" class="btn">×</label>
-      <p class="box">本日、<span id="numberDisplay"></span>人のネット応募が入りました</p>
-    </div>
-  </div>
-  <div id="countdownArea" class="countdown sp view_timer" data-end-date="2025/09/30 23:59:59">
-    <span class="countdown-tx deadline">締め切りまであと</span>
-    <span id="countdown-day" class="countdown-digit"></span><span class="txt">日</span>
-    <span id="countdown-hour" class="countdown-digit"></span><span class="txt">時間</span>
-    <span id="countdown-min" class="countdown-digit"></span><span class="txt">分</span>
-    <span id="countdown-sec" class="countdown-digit"></span><span class="txt">.</span>
-    <span id="countdown-millisec" class="countdown-digit millisecond"></span><span class="txt"> 秒</span>
-  </div>
-  <div id="countdownArea" class="countdown02 sp view_timer" data-start-date="2025/10/1 00:00:00">
-    <span class="countdown-tx deadline">締め切りまであと</span>
-    <span id="countdown02-day" class="countdown-digit"></span><span class="txt">日</span>
-    <span id="countdown02-hour" class="countdown-digit"></span><span class="txt">時間</span>
-    <span id="countdown02-min" class="countdown-digit"></span><span class="txt">分</span>
-    <span id="countdown02-sec" class="countdown-digit"></span><span class="txt">.</span>
-    <span id="countdown02-millisec" class="countdown-digit millisecond"></span><span class="txt"> 秒</span>
-  </div>
+  <?php get_template_part('template/countdown'); ?>
 </div>
 <main>
 <div class="header_sp sp">
