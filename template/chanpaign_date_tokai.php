c0cfa8e38ef5c454e32c0daa10a2ab2563a086e7
keito noguchi <noguchi@hug-inc.co.jp>
2025-12-27 15:09:46 +0900

    タイマー修正

--- template/chanpaign_date_tokai.php
+++ template/chanpaign_date_tokai.php
@@ -1,16 +1,18 @@
 <?php
-// 日付切替条件
-if (strtotime(date('Y-m-d H:i')) < strtotime('2025-12-15 15:00')) {
-    $month = 12;
-    $day   = 15;
-} else {
+$current_year = (int)date('Y');
+
+if (strtotime(date('Y-m-d H:i')) < strtotime($current_year . '-12-31 15:00')) {
     $month = 12;
     $day   = 31;
+    $target_year = $current_year;
+} else {
+    $month = 1;
+    $day   = 31;
+    $target_year = $current_year + 1;
 }
 
-// 曜日計算
 $week = ['日','月','火','水','木','金','土'];
-$w = $week[ date('w', strtotime("2025-$month-$day")) ];
+$w = $week[ date('w', strtotime("$target_year-$month-$day")) ];
 ?>
 
 <span class="date">
--- template/target_sp.php
+++ template/target_sp.php
@@ -1,5 +1,5 @@
-<?php if (strtotime(date('Y-m-d H:i')) < strtotime('2025-12-15 15:00')) { ?>
-  2025.12.15
-<?php } else { ?>
+<?php if (strtotime(date('Y-m-d H:i')) < strtotime('2025-12-31 15:00')) { ?>
   2025.12.31
+<?php } else { ?>
+  2026.1.31
 <?php } ?></p>
