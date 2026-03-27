7c9e21916a381877fab63ed684d89c7d31b2e7e9
keito noguchi <noguchi@hug-inc.co.jp>
2026-03-27 12:49:52 +0900

    26//3/26 キャンペーン日付修正

--- template/chanpaign_date.php
+++ template/chanpaign_date.php
@@ -1,5 +1,5 @@
-<?php if (strtotime(date('Y-m-d H:i')) < strtotime('2026-3-15 15:00')) { ?>
-  3/15
-<?php } else { ?>
+<?php if (strtotime(date('Y-m-d H:i')) < strtotime('2026-3-31 15:00')) { ?>
   3/31
+<?php } else { ?>
+  4/15
 <?php } ?>
